f846b492df7f8dc5a89f520988d6a7b56a402d75
Dev Sanghvi <devsanghvi06@gmail.com>
2024-05-23 12:51:12 +0530

    HMAC hashing of received payload

--- src/index.js
+++ src/index.js
@@ -8,8 +8,37 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+
+import { createHmac, timingSafeEqual } from 'node:crypto'; // used to hash the received payload
+import { Buffer } from 'node:buffer';
+
+//using the node js crypto library to hash the received payload
+function checkSignature(text,headers,githubsecrettoken){
+	// for computing the hash
+	const hmac=createHmac('sha256',githubsecrettoken); //hash obj
+	hmac.update(text);
+	const expectedSignature = hmac.digest('hex');
+	const actualSignature = headers.get('x-hub-signature-256');
+//signature comparison
+	const trusted=Buffer.from(`sha256=${expectedSignature}`, 'ascii');
+	const untrusted =  Buffer.from(actualSignature, 'ascii');
+
+	// checking for the byte length
+return trusted.byteLength==untrusted.byteLength 
+
+}
+
+async fetch(request, env, ctx) {
+	if(request.method !== 'POST') {
+	  return new Response('Try sending the POST request');
+	}
+	try {
+	  const rawBody = await request.text();
+
+	  if (!checkSignature(rawBody, request.headers, env.GITHUB_SECRET_TOKEN)) {
+		return new Response("Wrong password, try again", {status: 403});
+	  }
+	} catch (e) {
+	  return new Response(`Error:  ${e}`);
+	}
+  },
